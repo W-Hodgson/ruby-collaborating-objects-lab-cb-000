afe03b0685bdbb1bfa4eb798c0a11accfe892a82
Will Hodgson <will.hodgson@current-rms.com>
2019-08-06 12:29:19 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,20 @@
+require 'pry'
+class MP3Importer
+
+  attr_accessor :path, :files
+
+  def initialize(file_path)
+    @path = file_path
+  end
+
+  def files
+    @files = Dir.entries(@path).select do |file_name|
+      file_name.match(/\w+\.mp3/)
+    end
+  end
+
+  def import
+    
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+require 'pry'
+class Song
+
+  @@all = []
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+
+  def artist
+    @artist
+  end
+
+  def self.new_by_filename(filename)
+
+    song_name = filename.split('-')[1].strip
+    artist_name = filename.split('-')[0].strip
+
+    song = Song.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
+  end
+
+end
